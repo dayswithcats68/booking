3a3feba7685f4727c9d731fabfbf284865d8bd93
dayswithcats68 <dayswithcats68@gmail.com>
2026-08-07 17:36:19 +0800

    Configure booking data endpoint

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.CAT_STAY_CONFIG = Object.freeze({
   liffId: "2011017691-bZeYYgSL",
-  bookingEndpoint: "",
+  bookingEndpoint:
+    "https://script.google.com/macros/s/AKfycbymUca4DWx2eGiuKpqsGI5uLTYyL1Iofus6EHAxVzAtuj-51Px9HdhG_CUFjR0HZQts/exec",
 });
