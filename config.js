8a624413d5dd436ea0ab4af2240b806f04b9a293
dayswithcats68 <dayswithcats68@gmail.com>
2026-08-07 18:02:09 +0800

    Remove LIFF-only configuration

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.CAT_STAY_CONFIG = Object.freeze({
-  liffId: "2011017691-bZeYYgSL",
   bookingEndpoint:
     "https://script.google.com/macros/s/AKfycbymUca4DWx2eGiuKpqsGI5uLTYyL1Iofus6EHAxVzAtuj-51Px9HdhG_CUFjR0HZQts/exec",
 });
